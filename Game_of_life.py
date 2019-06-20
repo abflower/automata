55b459ecb96adc9e14a612d7c56a8a9df011a7f6
abellafiore <alessandro.bellafiore@zynstra.com>
2019-06-20 16:50:00 +0100

    Conway's Game of life

--- Game_of_life.py
+++ Game_of_life.py
@@ -1,3 +1,26 @@
+'''
+A simple implementation of Conway's 'Game of life'.
+The user can customise matrix dimension and number of iterations.
+The matrix is randomly populated.
+
+Copyright (c) 2019 Alessandro Bellafiore
+All rights reserved.
+
+Redistribution and use in source and binary forms are permitted
+provided that the above copyright notice and this paragraph are
+duplicated in all such forms and that any documentation,
+advertising materials, and other materials related to such
+distribution and use acknowledge that the software was developed
+by the authors. The name of the authors may not be used to endorse
+or promote products derived from this software without specific prior
+written permission.
+
+THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+'''
+
+
 import random
 from time import sleep
 
